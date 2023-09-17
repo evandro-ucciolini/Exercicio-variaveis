f1e4fdf3d0fb8422ba290ef69e98da8eed49dab1
unknown <evandro.ucciolini@outlook.com>
2023-09-17 14:48:21 -0300

    Exercício 10 realizado!

--- exercicios-backend-variaveis/10/delta.js
+++ exercicios-backend-variaveis/10/delta.js
@@ -1,2 +1,15 @@
+// Calculando delta
 
+//Considerando a equação de segundo grau abaixo:
 
+//$$ 5x^2 + 5x + 1 = 0 $$
+
+//$$ delta = 5^2 - 4 \* 5 \* 1 $$
+
+//$$ delta = 25 - 20
+
+//$$ delta = 5 $$
+
+const delta = 5 ** 2 - 4 * 5 * 1
+
+console.log(`O valor de delta é ${delta}.`)
