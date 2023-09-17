3ff95e13ed15449ca1f16337810b7165b589e4ec
unknown <evandro.ucciolini@outlook.com>
2023-09-17 11:15:38 -0300

    Atualizado. Coloquei o To.Fixed (2) para limitar os números após a vírgula.

--- exercicios-backend-variaveis/01/calculoIMC.js
+++ exercicios-backend-variaveis/01/calculoIMC.js
@@ -1,14 +1,14 @@
 // Cálculo do meu IMC:
 
- const meuPeso = 97.4  // P
- const minhaAltura = 1.98 // A
+const meuPeso = 97.4  // P
+const minhaAltura = 1.98 // A
 
 
-        //$$ I = \frac{P}{A^2} $$
-IMC = meuPeso/ (minhaAltura * minhaAltura) 
+//$$ I = \frac{P}{A^2} $$
+IMC = (meuPeso / (minhaAltura ** 2)).toFixed(2)
 
 
-console.log (IMC)
+console.log(`Meu IMC é: ${IMC}`)
 
 
 
