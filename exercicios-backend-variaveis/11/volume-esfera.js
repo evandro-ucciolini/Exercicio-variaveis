3fc26eeee056fd923e84d99ba6b1301b0d73ed12
unknown <evandro.ucciolini@outlook.com>
2023-09-17 15:06:00 -0300

    Exercício 11 realizado!

---
+++ exercicios-backend-variaveis/11/volume-esfera.js
@@ -0,0 +1,10 @@
+// Volume da Esfera
+
+const raio = 3
+
+
+//$$ v = \frac{4}{3} * 3 ^ 3 $$
+
+const volumeDaEsfera = (4 / 3) * raio ** 3
+
+console.log(`O volume de uma esfera de raio ${raio} é ${volumeDaEsfera} PI.`)
