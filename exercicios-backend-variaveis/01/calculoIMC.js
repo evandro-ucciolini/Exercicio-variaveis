a7a5f8bc533a16c1d8195e146cef108caca3c854
unknown <evandro.ucciolini@outlook.com>
2023-09-17 00:16:58 -0300

    Exercicio 01 realizado!

--- exercicios-backend-variaveis/01/calculoIMC.js
+++ exercicios-backend-variaveis/01/calculoIMC.js
@@ -1,3 +1,19 @@
+// Cálculo do meu IMC:
+
+ const meuPeso = 97.4  // P
+ const minhaAltura = 1.98 // A
+
+
+        //$$ I = \frac{P}{A^2} $$
+IMC = meuPeso/ (minhaAltura * minhaAltura) 
+
+
+console.log (IMC)
+
+
+
+
+
 
 
 
