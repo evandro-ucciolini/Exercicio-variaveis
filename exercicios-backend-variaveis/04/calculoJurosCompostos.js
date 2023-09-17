51790b510a43d4aee4c05968c85032723f96d425
unknown <evandro.ucciolini@outlook.com>
2023-09-17 11:22:30 -0300

    Atualizado com .ToFixed.

--- exercicios-backend-variaveis/04/calculoJurosCompostos.js
+++ exercicios-backend-variaveis/04/calculoJurosCompostos.js
@@ -1,11 +1,12 @@
 // Cálculo de juros Compostos
 
-const capital = 1.000
+const capital = 1000
 const taxaFixa = 0.125
 const tempo = 5
 
 //M = C(1 + i)^t 
-const montante = capital * (1 + 0.125) ** tempo
+const montante = (capital * (1 + 0.125) ** tempo).toFixed(2)
 
 
-console.log (`O valor do montante será de R$ ${montante}.`)
+
+console.log(`O valor do montante será de R$ ${montante}.`)
