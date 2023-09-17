9921a4c31f561520b7da21ea9a0817d58629467e
unknown <evandro.ucciolini@outlook.com>
2023-09-17 13:14:38 -0300

    exercício 08 realizado

---
+++ exercicios-backend-variaveis/08/calculoperimetro.js
@@ -0,0 +1,10 @@
+
+
+const raio = 1
+const comprimento = (2 * Math.PI * 1).toFixed(1)
+const area = (Math.PI * raio ** 2).toFixed(2)
+
+
+
+console.log(`A partir do raio ${raio} de um circulo, o tamanho aproximado do comprimento é de ${comprimento} e o aproximado da área é de ${area}.`)
+
