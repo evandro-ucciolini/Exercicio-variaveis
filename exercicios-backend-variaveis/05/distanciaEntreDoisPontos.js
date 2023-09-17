8c932df16056cf5540969f670f1f3a15d890e5de
unknown <evandro.ucciolini@outlook.com>
2023-09-17 12:17:06 -0300

    Exercício 04 realizado!

---
+++ exercicios-backend-variaveis/05/distanciaEntreDoisPontos.js
@@ -0,0 +1,16 @@
+// Distância entre dois pontos
+
+const pontoX1 = 1
+const pontoX2 = 1
+const pontoY1 = 1
+const pontoY2 = 4
+
+const distancia = Math.sqrt((pontoX2 - pontoX1) ** 2 + (pontoY2 - pontoY1) ** 2)
+
+
+
+console.log(` A distância entre dois pontoe é de ${distancia}.`)
+
+
+
+
