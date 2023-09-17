3f6d12f498d5c26c3e2bf8803ff32bde08893d4c
unknown <evandro.ucciolini@outlook.com>
2023-09-17 01:22:07 -0300

    Exercício 03 realizado

--- exercicios-backend-variaveis/03/descontoTenis.js
+++ exercicios-backend-variaveis/03/descontoTenis.js
@@ -1,1 +1,8 @@
+// Valor do tênis com desconto:
 
+const valorTotal = 129.99
+const meuDinheiro = 80
+const desconto = valorTotal - meuDinheiro
+const percentualDoDesconto = desconto *100 / valorTotal 
+
+console.log (` O vendedor terá que oferecer o percentual de ${percentualDoDesconto} % de desconto para o preço de tênis ficar em ${meuDinheiro} reais.`)
