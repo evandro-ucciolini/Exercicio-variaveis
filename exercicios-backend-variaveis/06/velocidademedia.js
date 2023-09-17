732f58932d042881d8311c489903d0ca926170e5
unknown <evandro.ucciolini@outlook.com>
2023-09-17 12:27:05 -0300

    Exercício 06 realizado

--- exercicios-backend-variaveis/06/velocidademedia.js
+++ exercicios-backend-variaveis/06/velocidademedia.js
@@ -3,5 +3,12 @@
 //speed = distance / time;
 //console.log(speed); // 50
 
+const distanciaPercorrida = 500
+const tempo = 10
+
+const velocidade = (distanciaPercorrida / tempo) * 3.6
+
+console.log(` A velocidade percorrida é de ${velocidade} km/h.`)
+
 
 
