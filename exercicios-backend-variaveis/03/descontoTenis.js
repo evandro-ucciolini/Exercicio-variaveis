3e2c40d340c67a6c3b9f23d689fdf59291b67c30
unknown <evandro.ucciolini@outlook.com>
2023-09-17 11:19:59 -0300

    Atualizado com o To.Fixed.

--- exercicios-backend-variaveis/03/descontoTenis.js
+++ exercicios-backend-variaveis/03/descontoTenis.js
@@ -3,6 +3,6 @@
 const valorTotal = 129.99
 const meuDinheiro = 80
 const desconto = valorTotal - meuDinheiro
-const percentualDoDesconto = desconto *100 / valorTotal 
+const percentualDoDesconto = (desconto * 100 / valorTotal).toFixed(2)
 
-console.log (` O vendedor terá que oferecer o percentual de ${percentualDoDesconto} % de desconto para o preço de tênis ficar em ${meuDinheiro} reais.`)
+console.log(` O vendedor terá que oferecer o percentual de ${percentualDoDesconto} % de desconto para o preço de tênis ficar em ${meuDinheiro} reais.`)
